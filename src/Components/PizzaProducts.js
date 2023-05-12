6f83ecd99fbbb53798d6f3e0edb225978931b21e
Gokul <gokulsethi69@gmail.com>
2023-05-12 18:20:07 +0530

    Add project.

--- src/Components/PizzaProducts.js
+++ src/Components/PizzaProducts.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import peproni from './assets/peproni.png'
+import Pizza from './Pizza'
+
 const PizzaProducts = () => {
+
+    const[products,setProducts]=React.useState([]);
+
+    React.useEffect(() => {
+        
+    },[products])
+
   return (
     <div className='container mx-auto pb-24'>
         <h1 className='text-lg font-bold my-8'>
             Products
         </h1>
         <div className='grid grid-cols-5 my-8 gap-24'>
-            <div>
-                <img src={peproni}/>
-                <div className='text-center'>
-                <h2 className='text-lg  font-bold py-2'>Havana Special</h2>
-                <span className='bg-gray-200 py-1 rounded-full text-sm px-4'>Small</span>
-                </div>
-                <div className='flex justify-between items-center  mt-4'>
-                    <span> ₹ 500 </span>
-                    <button className=' bg-yellow-500 py-1 px-4 rounded-full font-bold'> ADD </button>
-
-                </div>
-            </div>
+           <Pizza/>
+           <Pizza/>
+           <Pizza/>
+           <Pizza/>
         </div>
     </div>
   )
